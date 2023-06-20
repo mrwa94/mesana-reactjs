c8137cd636e5f8ac6049f1fd0765e57517f4b211
mrwaff4 <mreaff4@gmail.com>
2023-06-21 01:08:08 +0300

    v2

--- src/compontes/Store/StoreCard.js
+++ src/compontes/Store/StoreCard.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import { Card , Button  } from 'react-bootstrap'
+import { Card , Button , Offcanvas, CardImg  } from 'react-bootstrap'
 import ProductDetails from './ProductDetails'
+
+import { useState } from 'react'
+
+//import image to test 
+import  image2 from '../../assets/images/coffee.png'
+
+
+
 
 
 
@@ -8,15 +16,33 @@ import product from '../../assets/images/product.png'
 
 //icons
 import { HiShoppingCart } from "react-icons/hi2";
+import { BsCardText } from 'react-icons/bs'
 
 
 function StoreCard(props){
+  
+
+  // show a product in small menu 
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  
+  const handleShow = () => setShow(true);
+
+
+
+  const detailsHandel = () => {
+      
+    alert('hello marwa ..')
+   
+  }
 
   return (
 
 
         <div className = "card shadow mx-2 mt-5 mb-3  " 
-              style={{ width: '16rem' }}>
+              style={{ width: '16rem' }}
+          
+              >
 
 
           <img src ={props.imgProduct} 
@@ -29,14 +55,63 @@ function StoreCard(props){
             <p> {props.priceProduct}</p>
 
             <button 
-            // onClick={<ProductDetails/>} 
-            className = "primary-btn rounded w-100 shadow ">
+            variant="primary"
+            className = "primary-btn rounded w-100 shadow"
+            onClick={handleShow}
+            >
+              
+              
                أضف الى طلباتي
                <HiShoppingCart/>
               </button> 
           </div>
+    
 
+          {/* Order summary */}
+        <Offcanvas 
+        show={show} 
+         placement='end'>
+        <Offcanvas.Header > 
+        <Offcanvas.Title>متجر مسانا</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+
+          {/* import from API */}
           
+          <div className='h-25 row  ps-0 primary-btn'>
+               <div className='col w-50 h-50 '>
+               <img className='' src={image2}></img>
+               </div>
+             <div className='col'>
+              <h5 className=''>اسم المنتج</h5>
+              <h6 className=''>وصف الطلب </h6>
+             </div>
+
+             <div className='col'>
+             <button></button>
+             </div>
+
+          </div>
+          
+        </Offcanvas.Body>
+
+      <div className='text-center pb-3'>
+         <button className='primary-btn mx-3 rounded'
+                  onClick={handleClose}
+                   > متابعة التسوق
+           </button>
+
+
+          <button className='primary-btn rounded'
+                  // onClick={}
+                  > الشراء الآن 
+          </button>
+        </div>
+      </Offcanvas>
+
+      
+          
+
            </div>
 
     
